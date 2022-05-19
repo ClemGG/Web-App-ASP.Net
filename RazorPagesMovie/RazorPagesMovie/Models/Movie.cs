53ffd19e2eb829d16a06ce54380cf1db35c9f31a
Clement Guichard <c.guichard508@laposte.net>
2022-05-19 15:00:32 +0200

    added field validation

--- RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -5,20 +5,29 @@ namespace RazorPagesMovie.Models
 {
     public class Movie
     {
-        public int ID { get; set; } //required by the database for the primary key.
-        public string Title { get; set; } = string.Empty;
+        public int ID { get; set; }
 
-        //With this attribute:
-        //- The user isn't required to enter time information in the date field.
-        //- Only the date is displayed, not time information.
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Title { get; set; } = string.Empty;
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; } = string.Empty;
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; } = string.Empty;
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; } = string.Empty;
     }
 }
